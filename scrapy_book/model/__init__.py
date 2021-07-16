19fc1928e0f40d8a9b67496a6188f25c700861f0
Shadow Zhu <54241456+z-sq@users.noreply.github.com>
2021-07-16 12:42:49 +0800

    Update and rename scrapy_book/model/__init__.py to scrapy_book/模型/__init__.py

--- scrapy_book/model/__init__.py
+++
@@ -1,9 +0,0 @@
-from peewee import *
-
-db = MySQLDatabase("system_design", host="gointo.icu", port=3306, user="root",
-                   password="nxA5VNjoK4x4ATQ3HRXbgkoBhXH4k2FM")
-
-
-class BaseModel(Model):
-    class Meta:
-        database = db
